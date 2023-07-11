209559968e7893910a8cd64d638e89611ca481b4
chukwuka-emi <chukwuka@us.ftft.com>
2023-07-11 17:09:51 +0100

    updated property apis

---
+++ Migrations/20230711160908_updatedPropertyImageTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PropertyBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedPropertyImageTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FileId",
+                table: "PropertyImages",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileId",
+                table: "PropertyImages");
+        }
+    }
+}
